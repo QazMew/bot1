253954a51fabd78527381307e0f663a19b4bea57
QazMew <zemetskiys@gmail.com>
2022-01-05 21:34:13 -0800

    token removal

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const Discord = require("discord.js")
 require("dotenv").config()
 
-const TOKEN = "OTI3MzQ1OTE2MDk5MzIxOTE2.YdI4Lw.0bnunWH2rIvan5KqfKwB4o4VyeY"
-
 const client = new Discord.Client({
     intents: [
         "GUILDS",
@@ -21,3 +19,5 @@ client.on("messageCreate", (message) => {
 })
 
 client.login(process.env.TOKEN)
+
+// 
